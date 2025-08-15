0cda3dcb16506842c9df1974902e4804294f6f76
choismjames23 <choism23@naver.com>
2025-08-16 00:28:45 +0900

    궁수 캐릭터 클래스 생성

---
+++ character/archer.py
@@ -0,0 +1,8 @@
+from character.character_base import CharacterBase
+
+class Archer(CharacterBase):
+
+    def __init__(self, name, hp, attack_damage):
+        self.name = name
+        self.hp = hp
+        self.attack_damage = attack_damage
